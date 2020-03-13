b3a8ed3594750efbb52f9d444f56fb2843a44950
Vytautas <vytautasragaisis07@gmail.com>
2020-03-13 11:20:44 +0200

    first commit

--- app/src/main/java/com/example/soda_drinks_app/NewEntryActivity.java
+++ app/src/main/java/com/example/soda_drinks_app/NewEntryActivity.java
@@ -1,6 +1,9 @@
 package com.example.soda_drinks_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,14 @@ public class NewEntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_entry);
+
+        Button newEntryButton = findViewById(R.id.new_entry_button);
+        newEntryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoOrderActivity = new Intent(NewEntryActivity.this, SodaActivity.class);
+                startActivity(gotoOrderActivity);
+            }
+        });
     }
 }
